f082c7bab47828d6c89872988538747765f142a1
Alexdclark <Alexdclark93@gmail.com>
2019-01-31 17:36:33 -0600

    Updates to logic.

--- js/quotes.js
+++ js/quotes.js
@@ -1,4 +1,9 @@
 
+    if (bowser.msie && bowser.version <= 6) {
+        document.getElementById("Quote").innerHTML = "Open this page in something other than IE to see more!"
+    }
+
+    else{
 
 var quoteArray = [];
 var options = {
@@ -16,11 +21,6 @@ fetch('https://quotes-6343.restdb.io/rest/office-quotes', options).then(function
     var randomIndex = Math.trunc(Math.random() * quoteArray.length);
 
 
-    if (bowser.msie && bowser.version <= 6) {
-        document.getElementById("Quote").innerHTML = "Open this page in something other than IE to see more!"
-    }
-
-    else{
     //Setting the quote and speaker on page load
     document.getElementById("Quote").innerHTML = quoteArray[randomIndex].quote;
 
